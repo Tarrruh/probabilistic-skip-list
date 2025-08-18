e0838d75845d59539dda354a94ff68330b086e1e
duckyies <variyathabhinav@gmail.com>
2025-08-18 22:27:08 +0530

    complete skip list insertion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-mod probabilistic_skip_list_complicated_but_ill_finish_it_sometime;
+
 mod probabilistic_skip_list;
 
-use crate::probabilistic_skip_list_complicated_but_ill_finish_it_sometime::*;
+use crate::probabilistic_skip_list::*;
 fn main() {
-    println!("{}", Bound::Value(5) < Bound::Value(10));
-    println!("{:?}", SkipListNode::new(5,3));
-    let a = KeyValuePair("tome string idk", KeyValuePair("String", 1));
-    let b = KeyValuePair("some string idk", KeyValuePair("String", 1));
-    println!("{}", a.key() < b.key());
-    println!("{:?}", SkipListNode::new(a, 5))
+    // println!("{}", Bound::Value(5) < Bound::Value(10));
+    // println!("{:?}", SkipListNode::new(5,3));
+    // let a = KeyValuePair("tome string idk", KeyValuePair("String", 1));
+    // let b = KeyValuePair("some string idk", KeyValuePair("String", 1));
+    // println!("{}", a.key() < b.key());
+    // println!("{:?}", SkipListNode::new(a, 5))
+    // 
+    
+    let mut skip_list: ProbabilisticSkipList<i32> = ProbabilisticSkipList::new(1f32/2f32);
+    println!("{:?}", skip_list);
+    skip_list.insert(10);
+    skip_list.insert(5);
+    skip_list.insert(7);
+    println!("{:?}", skip_list);
 }
--- src/probabilistic_skip_list.rs
+++ src/probabilistic_skip_list.rs
@@ -1,10 +1,12 @@
 use std::io::{self, Read};
 use std::{fmt};
+use std::cmp::max;
 use std::fmt::Formatter;
 use std::ops::Neg;
 use std::rc::Rc;
+use rand::{random, Rng};
 
-const MAX_LEVEL: i32 = 20;
+const MAX_LEVEL: i32 = 20; //maybe change it to 16 idk compare later
 type NodeID = usize;
 pub trait KeyVal {
     type Key: Ord;
@@ -31,6 +33,8 @@ pub(crate) enum Bound<T> {
     PosInf,
     Null
 }
+
+
 
 impl<T> Bound<T> {
     pub fn value(&self) -> &T {
@@ -66,7 +70,7 @@ impl<T: KeyVal + Clone> SkipListNode<T> {
     pub fn new(data: T, level: usize) -> Self {
         SkipListNode {
             data: Bound::Value(data),
-            forwards: vec![None; level],
+            forwards: vec![None; level + 1usize],
         }
     }
 
@@ -80,7 +84,7 @@ impl<T: KeyVal + Clone> SkipListNode<T> {
     pub fn new_sentinel(bound: Bound<T>, level: usize) -> Self {
         SkipListNode {
             data: bound,
-            forwards: vec![None; level],
+            forwards: vec![None; level + 1usize],
         }
     }
 
@@ -104,11 +108,12 @@ pub struct ProbabilisticSkipList<T: KeyVal + Clone> {
     head: NodeID,
     free_list: Vec<NodeID>,
     nodes: Vec<SkipListNode<T>>,
+    promotion_chance: f32,
 }
 
-impl<T: KeyVal + Clone> ProbabilisticSkipList<T> {
+impl<T: KeyVal + Clone + PartialOrd> ProbabilisticSkipList<T> {
 
-    pub fn new() -> Self {
+    pub fn new(promotion_chance: f32) -> Self {
         let mut head = SkipListNode::new_sentinel(Bound::<T>::NegInf, MAX_LEVEL as usize);
         let tail = SkipListNode::new_sentinel(Bound::<T>::PosInf, MAX_LEVEL as usize);
         for i in 0..head.forwards.len() {
@@ -119,7 +124,70 @@ impl<T: KeyVal + Clone> ProbabilisticSkipList<T> {
             head: 0,
             free_list: vec![],
             nodes: vec![head, tail],
+            promotion_chance,
         }
     }
 
-}
+    pub fn insert(&mut self, data: T) -> Result<NodeID, &'static str> {
+        let index = self.allocate_index();
+        let max_level = self.get_max_level();
+        let mut node = SkipListNode::new(data.clone(), max_level as usize);
+        self.nodes[index] = node;
+        //store index in vec of size max_level
+        //then store index of that node in index in list
+        //update 4 level to new node so store it in in index 3.
+        // ok take level from 0 instead to save headachee
+        // do it before you forget
+        //size should be level + 1
+        //add equal check
+        //youll have to update same amount of pointers as the max level + 1
+        let mut updates = vec![0; max_level as usize + 1usize];
+        let mut curr_level = MAX_LEVEL - 1;
+        let mut curr_node = 0;
+        while curr_level >= 0 {
+
+            if let Some(node_index) = self.nodes.get(curr_node).expect("We shouldn't be here :( fix later").forwards[curr_level as usize] {
+                if self.nodes.get(node_index).expect("Invalid index given here").data > Bound::Value(data.clone()) {
+                    if updates.len() > curr_level as usize {
+                        updates[curr_level as usize] = curr_node as i32;
+                    }
+                    curr_level -= 1;
+
+                }
+                else {
+                    curr_node = self.nodes[curr_node].forwards[curr_level as usize].expect("Invalid id given")
+                }
+            }
+            else {
+                panic!("Shouldn't reach here");
+            }
+        }
+        for idx in 0..updates.len() {
+            let temp = self.nodes[updates[idx] as usize].forwards[idx];
+            self.nodes[index].forwards[idx] = temp;
+            self.nodes[updates[idx] as usize].forwards[idx] = Some(index);
+        }
+        println!("{:?}", updates);
+        Ok(index)
+    }
+
+    fn allocate_index(&mut self) -> NodeID {
+        if let Some(id) = self.free_list.pop() {
+            id
+        }
+        else {
+            let id = self.nodes.len();
+            self.nodes.push(SkipListNode::default());
+            id
+        }
+    }
+    
+    fn get_max_level(&self) -> i32 {
+        let mut level = 0;
+        let mut rng = rand::rng();
+        while rng.random() {
+            level += 1;
+        }
+        level
+    }
+ }
